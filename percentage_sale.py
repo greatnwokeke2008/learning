0fac4d005967c7b773099fa5e4cce495e6eb1412
Great Nwokeke <greatnwokekeg@gmail.com>
2024-11-28 13:07:26 -0600

    i am pushing the code i learnt today to github

---
+++ percentage_sale.py
@@ -0,0 +1,9 @@
+## we have a original price  
+# due to thankgiving we have 20% off the original
+## we find 20 percent of original price 
+# new price is = original price - 20 percent of the original price. 
+## the result is now our new price 
+original_price = float(input(" put in you price: "))
+twenty_percent_off = 0.2 * original_price
+new_price = original_price - twenty_percent_off
+print(new_price)
---
+++ practice.py
@@ -0,0 +1,4 @@
+reading_hours = int(input('put how many hours  you read  today:')) 
+Understanding_hours = float(input('write how long down to the second it took you to understand:'))
+total = reading_hours + Understanding_hours
+print('this is the total', total) 
