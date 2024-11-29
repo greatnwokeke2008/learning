ae380e19fe2f914e63c8061268d517b1c9301156
Great Nwokeke <greatnwokekeg@gmail.com>
2024-11-29 17:02:21 -0600

    this is what i learnt today, i learnt how to use remenders, integers division, pemdas. and i am pushing this code to git hub

---
+++ float_int.py
@@ -0,0 +1,15 @@
+## we are learning about opt in python 
+## operator in python are addition +, subtraction -, multiplication *, division /, integer division //,
+#  remenders %, and exponets **.  
+my_float_var = -4                                                                                                 
+my_sec_float_var = 2
+final_output =  my_float_var // my_sec_float_var 
+print(final_output)
+my_int = 15 
+my_int_two = 2
+divison_of_it = my_int % my_int_two
+print(divison_of_it)
+my_floatg_var = -3.1                                                                                          
+my_secc_float_var = 3
+final_lol_output =  my_floatg_var // my_secc_float_var 
+print(final_lol_output)
---
+++ pricedence.py
@@ -0,0 +1,6 @@
+outcome = (12.0 + 6.0) / 3.0
+print(outcome) 
+value = (3 % 2 ) * (6 / 3) / 2
+print(value)
+exponet = 4**2
+print(exponet) 
