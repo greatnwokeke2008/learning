d180796555f6aca7990dfeb1c95f04c3ed77c8d3
Great Nwokeke <greatnwokekeg@gmail.com>
2024-11-30 15:53:14 -0600

    i learnt today how to make code more neater by seprating it or putting it on a new  line.

---
+++ math_formula_python.py
@@ -0,0 +1,19 @@
+## when an operation are done on 2 intergr value the result will be a integer. 
+## same thing but with a float 
+## if it done on both a interger and a float the result will be temporay convertaed to a float aswell as the result 
+## what i just stated here are implicit conversions    
+## this symbol \ help of continue your mathmathical expression but in a  new line  
+float_value = 2.6 
+print(float_value)
+int_value = int(float_value)
+print(int_value)
+var_x =  (float_value + int_value ** 
+ float_value)
+print(var_x)
+
+## supressing the  print  funcution, ending new line . 
+print('one', end= "^")
+print('two',  end= "^")
+print('three')    
+print('one','two','three', sep='---')
+print('one\ntwo\nthree')
